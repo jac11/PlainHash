43f7bd9e36923aa8dc178e45bde70c7634003847
jac11 <rootx1982@gmail.com>
2021-05-03 21:49:39 -0400

    Update Info.py

--- Package_Hash/Info.py
+++ Package_Hash/Info.py
@@ -19,7 +19,7 @@ class Info:
              print ('[*] SHA_256         [*] SHA3_256')
              print ('[*] SHA_384         [*] SHA3_384   ')
              print ('[*] SHA_512         [*] SHA3_512')            
-             print ('[*] BLAKE2b         [*] BLAKE2c'+W+'\n')          
+             print ('[*] BLAKE2b         [*] BLAKE2c'+W+'\n')            
              print (P+'[+] With non-Crypto Hsah Copy String Hash  Into File') 
              print ('[+] Same Like : 5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9')
              print ('[*] then Use -r Option or --read To give File Path' )
@@ -27,9 +27,9 @@ class Info:
              print ('[+] or Use -H with Hash String')
              print ('[+] Example : ./PlainHash.py -H 5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9 -w /home/wordlist'+W+'\n')               
              print (Y+'='*35,'\n\n','[*] Salt Hash Support :','\n',('*'*25),'\n')
-             print ('[*] MD5-CRYPT       [*] BCRYPT-[Y]')
-             print ('[*] SHA1-CRYPT      [*] SHA256-CRYPT')
-             print ('[*] SHA512-CRYPT'+W+'\n')              
+             print ('[*] MD5-CRYPT        [*] SHA1-CRYPT  ')
+             print ('[*] SHA256-CRYPT     [*] SHA512-CRYPT  ')
+             print ('[*] BCRYPT-[Y]       [*] BCRYPT-[2Y] '+W+'\n')              
              print (P+'[+] With "Crypt Hash" Copy Hash Into File') 
              print ('[+] Example : $6$efxS7PCQU0SZi33L$H7sWCUQJ0dDBKwSZmxwADtp6D553OyjFRUfA3PKnf4JAT625jiRvDBFUTB2501CLCDzNlbjkCqM4PFJsxV9Qx/')
              print ('[*] then Use -r Option or --read To give File Path' )
@@ -41,9 +41,17 @@ class Info:
              print ('[+] or 8846F7EAEE8FB117AD06BDD830B7586C')
              print ('[+] Example : ./PlainHash.py - r /home/hash.txt -w /home/wordlist')
              print ('[+] or Use -H with NTLM MD4 Hash')
-             print ('[+] Example : ./PlainHash.py -H 8846F7EAEE8FB117AD06BDD830B7586C -w /home/wordlist'+W)              
+             print ('[+] Example : ./PlainHash.py -H 8846F7EAEE8FB117AD06BDD830B7586C -w /home/wordlist'+W+'\n')      
+             
+                    
              print (Banner)      
              exit()
+    print_info()
+if __name =='__main__':
+    Info()     
+    
+ 
+ 
     print_info()
 if __name =='__main__':
     Info()     
